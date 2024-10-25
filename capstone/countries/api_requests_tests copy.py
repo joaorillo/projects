10c7bdc26df35d563978d18efb0373d5eee4a5ac
joaorillo <joaopedronerillo@gmail.com>
2024-10-25 14:27:33 -0300

    restarting repository

--- capstone/countries/api_requests_tests copy.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://countriesnow.space/api/v0.1/countries/population"
-
-data = {"country": "nigeria"}
-
-response = requests.post(url, data=data)
-
-print(response.text)
